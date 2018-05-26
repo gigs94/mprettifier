285739d0a79c24a18734083d2d68b56553b0f851
daniel gregory <gigs94@gmail.com>
2018-05-26 08:52:13 -0500

    added setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='mpretty',
+      version='1.0',
+      description='Number Prettifier',
+      author='daniel gregory',
+      author_email='gigs94@gmail.com',
+      url='https://github.com/gigs94/mpretty',
+      py_modules=['mpretty'],
+     )
