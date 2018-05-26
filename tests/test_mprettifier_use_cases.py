58005ef26330d99e1b6db4e262d6d0cfa579363c
daniel gregory <gigs94@gmail.com>
2018-05-25 23:00:31 -0500

    casting the return to a string for the assertion made everything work

--- tests/test_mprettifier_use_cases.py
+++ tests/test_mprettifier_use_cases.py
@@ -4,16 +4,16 @@ from mpretty import mpretty
 class test_mprettifier_use_cases(unittest.TestCase):
 
     def test_even_million(self):
-        self.assertEqual(mpretty('1000000'), '1M')
+        self.assertEqual(str(mpretty('1000000')), '1M')
 
     def test_non_even_million(self):
-        self.assertEqual(mpretty('2500000.34'), '2.5M')
+        self.assertEqual(str(mpretty('2500000.34')), '2.5M')
 
     def test_up_to_1k(self):
-        self.assertEqual(mpretty('532'), '532')
+        self.assertEqual(str(mpretty('532')), '532')
 
     def test_billion(self):
-        self.assertEqual(mpretty('1123456789'), '1.1B')
+        self.assertEqual(str(mpretty('1123456789')), '1.1B')
 
 if __name__ == '__main__':
     unittest.main()
