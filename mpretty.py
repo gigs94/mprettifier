e9c4683d2d571fdc14ab89d112e649dfa3060ef1
daniel gregory <gigs94@gmail.com>
2018-05-25 21:16:54 -0500

    added first implementation, tests still fail

---
+++ mpretty.py
@@ -0,0 +1,46 @@
+
+class mpretty:
+    """turn numbers into easy to read abbreviations"""
+
+    def __init__(self, number):
+        """Return a mprettifier object and set number to number"""
+        self.number = _num(number)
+        self.prettier_number = _prettify()
+
+    def __repr__(self):
+        return self.prettier_number
+
+    def number(self):
+        return self.number
+
+    def pretty_number(self):
+        return self.prettier_number 
+
+    def _num(s):
+        try:
+            return int(s)
+        except ValueError:
+            return float(s)
+
+    def _prettify(self):
+        """convert number to pretty form"""
+        modifier = ""
+        divider = 1
+        if self.number < 1000000:
+            # no changes
+            pass
+        elif self.number < 1000000000:
+            # millions changes
+            modifier = "M"
+            divider = 1000000
+        elif self.number < 1000000000000:
+            # billions changes
+            modifier = "M"
+            divider = 1000000000
+        else:
+            # trillions changes
+            modifier = "T"
+            divider = 1000000000000
+
+        return str(self.number/divider)+modifier
+
